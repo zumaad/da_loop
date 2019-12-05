be346e531566d0588014e74254e3e6e11b78b043
zumaad <khanzumaad@gmail.com>
2019-12-05 17:19:08 -0500

    event loop working with timed tasks

--- eventloop.py
+++ eventloop.py
@@ -2,6 +2,7 @@ from selectors import DefaultSelector
 from queue import Queue
 from typing import Callable
 import heapq
+import datetime
 
 
 class PriorityQueue:
@@ -18,18 +19,55 @@ class Task:
     pass
 
 class TimedTask:
-    pass
+    def __init__(self, delay):
+        self.delay = delay
+        self.delay_time = datetime.timedelta(seconds=delay)
+        self.start_time = datetime.datetime.now()
+        self.end_time = self.start_time + self.delay_time
+    
+    def is_complete(self) -> bool:
+        return datetime.datetime.now() > self.end_time
+    
+    def __str__(self):
+        return str(vars(self))
 
 class EventLoop:
     def __init__(self):
         self.task_queue = Queue()
+        self.coroutines = []
+        self.task_to_coroutine = {}
 
-    def register(func: Callable):
-        task_queue.put(func)
+    def register(self, func: Callable):
+        coroutine = func()
+        # coroutine.send(None)
+        self.coroutines.append(coroutine)
     
     def loop(self):
+        for coroutine in self.coroutines:
+            task = next(coroutine)
+            self.task_to_coroutine[task] = coroutine
+        
         while True:
-            try:
-                task = self.task_queue.get_nowait()
-                next(task)
-    
+            for task, coroutine in list(self.task_to_coroutine.items()):
+                if task.is_complete():
+                    new_task = next(coroutine)
+                    if new_task:
+                        self.task_to_coroutine[new_task] = coroutine
+                    del self.task_to_coroutine[task]
+                        
+def coro1():
+    while True:
+        print("hello")
+        yield TimedTask(5)
+
+def coro2():
+    while True:
+        print("bye")
+        yield TimedTask(1)
+
+
+if __name__ == "__main__":
+    event_loop = EventLoop()
+    event_loop.register(coro1)
+    event_loop.register(coro2)
+    event_loop.loop()
